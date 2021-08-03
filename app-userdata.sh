fb2256cb177322f1d16000661390e566be63812c
vivektiwary1992 <77327606+vivektiwary1992@users.noreply.github.com>
2021-08-03 12:53:22 +0530

    Update app-userdata.sh

--- app-userdata.sh
+++ app-userdata.sh
@@ -4,3 +4,14 @@ ami_id=`echo $artifact_id | cut -d ':' -f2`
 #echo 'variable "ami_id" { default = "'${ami_id}'" }' > varami.tf
 terraform init -var ami=”$ami_id”
 terraform apply -var ami=”$ami_id” -input=false -auto-approve 
+
+
+#!bin/bash
+artifact_id=`packer build -machine-readable manifest11.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+ami_id=`echo $artifact_id | cut -d ':' -f2`
+#echo 'variable "ami_id" { default = "'${ami_id}'" }' > varami.tf
+#sleep 60
+set | grep ami_id
+export ami_id
+terraform init -var ami= $ami_id
+terraform apply -var ami= $ami_id -input=false -auto-approve
