73d0c2572b1aa282ede20d87b2b7f7dfa77cc7c0
vivektiwary1992 <77327606+vivektiwary1992@users.noreply.github.com>
2021-08-03 12:54:00 +0530

    Update app-userdata.sh

--- app-userdata.sh
+++ app-userdata.sh
@@ -1,9 +1,9 @@
 #!bin/bash
-artifact_id=`packer build -machine-readable manifest11.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-ami_id=`echo $artifact_id | cut -d ':' -f2`
+#artifact_id=`packer build -machine-readable manifest11.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+#ami_id=`echo $artifact_id | cut -d ':' -f2`
 #echo 'variable "ami_id" { default = "'${ami_id}'" }' > varami.tf
-terraform init -var ami=”$ami_id”
-terraform apply -var ami=”$ami_id” -input=false -auto-approve 
+#terraform init -var ami=”$ami_id”
+#terraform apply -var ami=”$ami_id” -input=false -auto-approve 
 
 
 #!bin/bash
